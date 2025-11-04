e2b4452afadff3622690418396f107feafa22064
Stephane <sgobin@campus-eni.fr>
2025-11-04 13:49:19 +0100

    cave à vin - initialisation du projet

---
+++ src/main/java/fr/eni/caveavin/CaveavinApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.caveavin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaveavinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaveavinApplication.class, args);
+    }
+
+}
